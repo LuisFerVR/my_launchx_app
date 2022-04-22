7b5e745197cdc44fb708691677b396a9f5b2e1f6
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-04-22 13:12:47 -0500

    Lo tenia vacio para subir

---
+++ app/missionCommander.js
@@ -0,0 +1,7 @@
+class MissionCommander{
+    constructor(name){
+        this.name = name;
+    }
+}
+module.exports = MissionCommander;
+// Esta línea nos permite exportar nuestra clase
---
+++ index.js
@@ -0,0 +1,9 @@
+import MissionCommander from "./app/missionCommander";
+
+const M1 = new MissionCommander("Carlo");
+const M2 = new MissionCommander("Gilmar");
+const M3 = new MissionCommander("Luis");
+
+console.log(M1.name);
+console.log(M2.name);
+console.log(M3.name);
